1ff5c9d054e970d50f6355293c144b9d38904632
rsc398 <radio@rsc398.net>
2020-05-15 23:58:47 +0900

    KAKKO BGM2

---
+++ KAKKO_BGM2/kakkoBGM/kakkoBGM.ino
@@ -0,0 +1,25 @@
+void setup()
+{
+  for(int i=0; i<2; i=i+1) {
+  tone(8, 1220);
+  delay(200);
+  noTone(8);
+  delay(200);
+  tone(8, 980);
+  delay(600);
+  noTone(8);
+  delay(500);
+    tone(8, 1220);
+  delay(200);
+  noTone(8);
+  delay(200);
+  tone(8, 980);
+  delay(600);
+  noTone(8);
+  delay(500);
+  }
+}
+void loop()
+{
+
+}
