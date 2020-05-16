b15f3e40b5f8888bbeaa873b9145fb62abf4aa97
rsc398 <radio@rsc398.net>
2020-05-16 23:57:28 +0900

    TYAIMU BGM

---
+++ TYAIMU_BGM/TYAIMU_BGM/TYAIMU_BGM.ino
@@ -0,0 +1,48 @@
+void setup()
+{
+    tone(8, 698);
+    delay(800);
+    tone(8, 880);
+    delay(800);
+    tone(8, 784);
+    delay(800);
+    tone(8, 523);
+    delay(800);
+    noTone(8);
+    delay(800);
+    tone(8, 698);
+    delay(800);
+    tone(8, 784);
+    delay(800);
+    tone(8, 880);
+    delay(800);
+    tone(8, 698);
+    delay(1200);
+    noTone(8);
+    delay(400);
+    tone(8, 880);
+    delay(800);
+    tone(8, 698);
+    delay(800);
+    tone(8, 784);
+    delay(800);
+    tone(8, 523);
+    delay(800);
+    noTone(8);
+    delay(800);
+    tone(8, 523);
+    delay(800);
+    tone(8, 784);
+    delay(800);
+    tone(8, 880);
+    delay(800);
+    tone(8, 698);
+    delay(1200);
+    noTone(8);
+    delay(400);
+}
+
+void loop()
+{
+
+}
