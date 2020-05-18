f58c741f83cece3dfeb5839d857a564090d26818
rsc398 <radio@rsc398.net>
2020-05-18 22:47:27 +0900

    NHK BGM

---
+++ NHK_BGM/NHK_BGM/NHK_BGM.ino
@@ -0,0 +1,21 @@
+void setup()
+{
+  tone(8, 440);
+  delay(100);
+  noTone(8);
+  delay(900);
+  tone(8, 440);
+  delay(100);
+  noTone(8);
+  delay(900);
+  tone(8, 440);
+  delay(100);
+  noTone(8);
+  delay(900);
+  tone(8, 880);
+  delay(1500);
+  noTone(8);
+}
+void loop()
+{
+}
