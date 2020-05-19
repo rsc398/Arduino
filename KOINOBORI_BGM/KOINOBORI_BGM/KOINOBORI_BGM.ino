42c247886501e8650e44daedaac16935d8c9aac5
rsc398 <radio@rsc398.net>
2020-05-19 22:49:47 +0900

    KOINOBORI BGM

---
+++ KOINOBORI_BGM/KOINOBORI_BGM/KOINOBORI_BGM.ino
@@ -0,0 +1,29 @@
+void setup(){
+  tone(8, 330, 300);
+  delay(300);
+  tone(8, 294, 300);
+  delay(300);
+  tone(8, 262, 600);
+  delay(600);
+  tone(8, 294, 600);
+  delay(600);
+  tone(8, 330, 600);
+  delay(600);
+  tone(8, 440, 600);
+  delay(600);
+  tone(8, 392, 300);
+  delay(600);
+  tone(8, 330, 300);
+  delay(300);
+  tone(8, 330, 300);
+  delay(300);
+  tone(8, 330, 600);
+  delay(600);
+  tone(8, 294, 300);
+  delay(300);
+  tone(8, 262, 300);
+  delay(300);
+  tone(8, 294, 600);
+}
+void loop(){
+}
