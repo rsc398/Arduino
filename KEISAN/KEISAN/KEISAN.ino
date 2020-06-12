a1975af9cebc5c01e14e0f14b6a84b7a51ee2227
rsc398 <radio@rsc398.net>
2020-06-12 23:16:06 +0900

    KEISAN

---
+++ KEISAN/KEISAN/KEISAN.ino
@@ -0,0 +1,10 @@
+void setup(){
+Serial.begin(9600);
+Serial.print("5+5=");Serial.println(5+5);
+Serial.print("5-5=");Serial.println(5-5);
+Serial.print("5*5=");Serial.println(5*5);
+Serial.print("5/5=");Serial.println(5/5);
+Serial.print("5%5=");Serial.println(5%5);
+}
+void loop(){
+}
