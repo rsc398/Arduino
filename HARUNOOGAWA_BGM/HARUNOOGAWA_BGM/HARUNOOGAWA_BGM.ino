05242b8cb70c086579cefb2381cd33a07cbe8b57
rsc398 <radio@rsc398.net>
2020-06-13 20:45:58 +0900

    HARUNOOGAWA BGM

---
+++ HARUNOOGAWA_BGM/HARUNOOGAWA_BGM/HARUNOOGAWA_BGM.ino
@@ -0,0 +1,41 @@
+void setup() {
+    tone(12, 330) ;
+    delay(750) ;
+    tone(12, 392) ;
+    delay(750) ;
+    tone(12, 440) ;
+    delay(750) ;
+    tone(12, 392) ;
+    delay(750) ;
+    tone(12, 330) ;
+    delay(750) ;
+    tone(12, 392) ;
+    delay(750) ;
+    tone(12, 523) ;
+    delay(730) ;
+    noTone(12) ;
+    delay(20) ;
+    tone(12, 523) ;
+    delay(750) ;
+    tone(12, 440) ;
+    delay(730) ;
+    noTone(12) ;
+    delay(20) ;
+    tone(12, 440) ;
+    delay(750) ;
+    tone(12, 392) ;
+    delay(750) ;
+    tone(12, 330) ;
+    delay(750) ;
+    tone(12, 262) ;
+    delay(750) ;
+    tone(12, 294) ;
+    delay(750) ;
+    tone(12, 330) ;
+    delay(750) ;
+    noTone(12) ;
+}
+
+void loop() {
+
+}
