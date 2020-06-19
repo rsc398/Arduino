c1a5d89fe52fc02456bb3ef17e132c7050188c70
rsc398 <radio@rsc398.net>
2020-06-19 21:31:27 +0900

    AP CONNECT

---
+++ AP_CONNECT/AP_CONNECT/AP_CONNECT.ino
@@ -0,0 +1,38 @@
+#include <HttpClient_ESP8266_AT.h>
+const byte rxPin = 2; // Wire this to Tx Pin of ESP8266
+const byte txPin = 3; // Wire this to Rx Pin of ESP8266
+HttpClient_ESP8266_AT httpClient(rxPin, txPin);
+
+void setup() {
+     Serial.begin(9600);
+    while(true) {
+        if(httpClient.statusAT()) {
+            Serial.println("AT status OK");
+            break;
+        }
+        else {
+            Serial.println("AT status NOT OK");
+        }
+        delay(1000);
+    }
+    while(true) {
+        if(httpClient.connectAP("SSID", "PASSWORD")) {
+            Serial.println("Successfully connected to an AP");
+            break;
+        }
+        else {
+            Serial.println("Failed to connected to an AP. retrying...");
+        }
+        delay(1000);
+    }
+}
+
+void loop() {
+    if(httpClient.statusWiFi()) {
+        Serial.println("WiFi status OK");
+    }
+    else {
+        Serial.println("WiFi status NOT OK");
+    }
+    delay(1000);
+}
