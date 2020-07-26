3ab2aa614fbc17c0487ef1ca9efd918b4893a766
rsc398 <radio@rsc398.net>
2020-07-26 23:08:28 +0900

    433MHZ TX RX

---
+++ 433MHZ_TX_RX/433MHZ_RX/433MHZ_RX.ino
@@ -0,0 +1,20 @@
+#include <RH_ASK.h>
+#include <SPI.h> // Not actualy used but needed to compile
+RH_ASK driver;
+void setup()
+{
+    Serial.begin(9600);
+    if (!driver.init())
+         Serial.println("init failed");
+}
+void loop()
+{
+    uint8_t buf[12];
+    uint8_t buflen = sizeof(buf);
+    if (driver.recv(buf, &buflen))
+    {
+      int i;
+      Serial.print("Message: ");
+      Serial.println((char*)buf);         
+    }
+}
